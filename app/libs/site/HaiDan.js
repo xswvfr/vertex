86a739f7c313fe0417a21ab1ca8fdf76154eb1cc
MH <2@2.com>
2023-01-21 19:00:27 +0800

    add HaiDan/NicePT
    add HaiDan/NicePT

---
+++ app/libs/site/HaiDan.js
@@ -0,0 +1,33 @@
+const util = require('../util');
+
+class Site {
+  constructor () {
+    this.name = 'HaiDan';
+    this.url = 'https://www.haidan.video/';
+  };
+
+  async getInfo () {
+    const info = {};
+    const document = await this._getDocument(this.index, false, 10);
+    // 用户名
+    info.username = document.querySelector('a[href*=userdetails] b').innerHTML;
+    // uid
+    info.uid = +document.querySelector('a[href*=userdetails]').href.match(/id=(\d+)/)[1];
+    // 上传
+    info.upload = document.querySelector('font[class=color_uploaded]').nextSibling.nodeValue.trim().replace(/(\w)B/, '$1iB');
+    info.upload = util.calSize(...info.upload.split(' '));
+    // 下载
+    info.download = document.querySelector('font[class=color_downloaded]').nextSibling.nodeValue.trim().replace(/(\w)B/, '$1iB');
+    info.download = util.calSize(...info.download.split(' '));
+    // 做种
+    info.seeding = +document.querySelector('img[class=arrowup]').nextSibling.nodeValue.trim();
+    // 下载
+    info.leeching = +document.querySelector('img[class=arrowdown]').nextSibling.nodeValue.trim();
+    // 做种体积
+    const seedingDocument = await this._getDocument(`${this.index}getusertorrentlistajax.php?userid=${info.uid}&type=seeding`, true);
+    const seedingSize = (seedingDocument.match(/总大小\uff1a(\d+\.\d+ [KMGTP]B)/) || [0, '0 B'])[1].replace(/([KMGTP])B/, '$1iB');
+    info.seedingSize = util.calSize(...seedingSize.split(' '));
+    return info;
+  };
+};
+module.exports = Site;
